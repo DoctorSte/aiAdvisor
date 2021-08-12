a902e1a4ca063d9b0a3035c9a74b2b7c061d93a6
DoctorSte <st.dobrescu@gmail.com>
2021-08-12 15:51:00 +0100

    second commit

---
+++ pages/Components/contact.js
@@ -0,0 +1,182 @@
+import React, { useState, useRef } from "react";
+
+const formFields = ["name", "email", "message"];
+
+export default function Contact() {
+  /* const [formData, setFormData] = useState({}); */
+  const [message, setMessage] = useState("");
+  const choices = useRef([]);
+  /*
+  const handleInput = (e) => {
+    const copyFormData = { ...formData };
+    copyFormData[e.target.name] = e.target.value;
+    setFormData(copyFormData);
+    console.log(copyFormData);
+  }; */
+
+  const onChoiceChange = ({ target: { checked, name } }) => {
+    if (checked) choices.current.push(name);
+    else if (choices.current.includes(name)) {
+      choices.current = choices.current.filter((choice) => choice !== name);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const {
+      target: { elements },
+    } = e;
+    console.log(elements);
+
+    const payload = formFields.reduce((acc, field) => {
+      acc[field] = elements[field].value;
+      return acc;
+    }, {});
+
+    payload["choice"] = choices.current;
+    console.log(payload);
+    sendData(payload);
+  };
+
+  const sendData = async (payload) => {
+    try {
+      const response = await fetch(
+        "https://v1.nocodeapi.com/doctorste/airtable/SqlAObBeMPyuKlYk?tableName=contact",
+        {
+          method: "post",
+          body: JSON.stringify([payload]),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const json = await response.json();
+      console.log("Success:", JSON.stringify(json));
+      setMessage("This Worked!");
+    } catch (error) {
+      console.error("Error:", error);
+      setMessage("What the hell");
+    }
+  };
+
+  return (
+    <form
+      className=" pt-6 pb-8 mb-4"
+      id="contact"
+      name="contact"
+      required
+      onSubmit={onSubmit}
+    >
+      <input
+        id="name"
+        name="name" // name should matched with your airtable table field
+        type="text"
+        placeholder="Name"
+        required
+        className="shadow appearance-none border-2 border-gray-500 rounded w-full py-2 px-3 mb-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-green-300"
+
+        // onChange={handleInput}
+      />
+      <input
+        id="email"
+        name="email" // name should matched with your airtable table field
+        type="email"
+        placeholder="Email"
+        required
+        className="shadow appearance-none border-2 border-gray-500 rounded w-full py-2 px-3 mb-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-green-300"
+        // onChange={handleInput}
+      />
+      <textarea
+        id="message"
+        name=" message" // name should matched with your airtable table field
+        placeholder="Message"
+        className="shadow appearance-none border-2 border-gray-500 rounded w-full py-2 px-3 mb-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-green-300"
+        // onChange={handleInput}
+      />
+
+      <div className="flex space-x-4 ">
+        <label>
+          <input
+            type="checkbox"
+            name="1"
+            id="choice_1"
+            onChange={onChoiceChange}
+            class="form-checkbox h-5 w-5 peer transition duration-100 appearance-none "
+          />
+          <img
+            className="z-0 border-2 rounded peer-checked:border-blue-500 transition duration-100 cursor-pointer"
+            src="https://picsum.photos/seed/1/100"
+            alt=""
+          />
+          <p className="text-black peer-checked:text-blue-500 transition duration-100">
+            Choice 1
+          </p>
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            name="2"
+            id="choice_2"
+            onChange={onChoiceChange}
+            class="form-checkbox h-5 w-5 peer transition duration-100 appearance-none "
+          />
+          <img
+            className=" border-2 rounded peer-checked:border-blue-500 transition duration-100 cursor-pointer"
+            src="https://picsum.photos/seed/2/100"
+            alt=""
+          />
+          <p className="text-black peer-checked:text-blue-500 transition duration-100">
+            Choice 2
+          </p>
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            name="3"
+            id="choice_3"
+            onChange={onChoiceChange}
+            class="form-checkbox h-5 w-5 peer transition duration-100 appearance-none "
+          />
+          <img
+            className=" border-2 rounded peer-checked:border-blue-500 transition duration-100 cursor-pointer"
+            src="https://picsum.photos/seed/3/100"
+            alt=""
+          />
+          <p className="text-black peer-checked:text-blue-500 transition duration-100">
+            Choice 3
+          </p>
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            name="4"
+            id="choice_4"
+            onChange={onChoiceChange}
+            class="form-checkbox h-5 w-5 peer transition duration-100 appearance-none "
+          />
+          <img
+            className=" border-2 rounded peer-checked:border-blue-500 transition duration-100 cursor-pointer"
+            src="https://picsum.photos/seed/4/100"
+            alt=""
+          />
+          <p className="text-black peer-checked:text-blue-500 transition duration-100">
+            Choice 4
+          </p>
+        </label>
+      </div>
+      <br />
+      <div className="flex justify-between">
+        <input
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          name="submit"
+          type="submit"
+          value="Send"
+        />
+        <p className="text-black pt-2"> {message}</p>
+      </div>
+    </form>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,58 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import "tailwindcss/tailwind.css";
 import ReactPlayer from "react-player";
 import React, { useEffect, useState } from "react";
+import Contact from "./Components/contact";
+import Navbar from "./Components/navbar";
 
 export default function Home() {
   const [isPlaying, setIsPlaying] = useState(true);
   return (
-    <div className={styles.container}>
+    <div className=" min-w-full ">
       <Head>
         <title>AI Advisor</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="container p-44 max-w-7xl">
-        <div className="grid grid-flow-col grid-cols-2 gap-3">
+      <Navbar />
+      <main className="flex justify-center items-center min-w-full">
+        <div className="grid grid-flow-col grid-cols-2 gap-3 p-20 max-w-4xl">
           <div>
             <h1 className="text-5xl font-bold pb-6 text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-red-600">
               Get advice for your business idea!
             </h1>
-
             <p className="text-sm self-center pb-7">
               Enter your idea below and hit submit:
             </p>
+            <Contact />
           </div>
-          <div className="rounded-md overflow-hidden border-2 border-red-300 ">
+          <div className="rounded-md overflow-hidden border-2 border-red-300 bg-black ">
             <ReactPlayer
               url="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
               playing={isPlaying}
               muted={true}
               controls={true}
+              width={"100%"}
+              height={"100%"}
+              loop={true}
+              className=""
             />
           </div>
         </div>
       </main>
 
-      <footer className="min-w-full flex justify-center px-44 border-t-2">
+      <footer className="min-w-full flex justify-center border-t-2 items-center text-gray-500 text-center text-xs p-10">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-          className="p-4"
+          className=" "
         >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by
+          <img
+            src="/Ellipse 1.svg"
+            alt="Vercel Logo"
+            className=" w-10 self-center"
+          />
         </a>
       </footer>
     </div>
